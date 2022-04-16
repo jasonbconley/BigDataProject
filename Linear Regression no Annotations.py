dafedb31af23a3c5d53c9b2f998bc587defeea7c
yonitaweiszhauz <yonita.weiszhauz@gmail.com>
2022-04-16 16:02:30 -0400

    Added Pearson's r calculation

--- Linear Regression no Annotations.py
+++ Linear Regression no Annotations.py
@@ -1,6 +1,7 @@
 # Import the packages and classes needed in this example:
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from scipy import stats
 import matplotlib.pyplot as plt
 import queryTree
 
@@ -10,31 +11,36 @@ x1,y1,z1 = queryTree.queryTheTree()
 x = np.array(x1).reshape((-1, 1))
 y = np.array(y1)
 z = an= z1
+pX = np.array(x1) # just to be used for the Pearson's coefficient calculation
 
 # Create an instance of a linear regression model and fit it to the data with the fit() function:
 model = LinearRegression().fit(x,y) 
 
 # The following section will get results by interpreting the created instance: 
 
-# Obtain the coefficient of determination by calling the model with the score() function, then print the coefficient:
-r_sq = model.score(x, y)
-print('coefficient of determination:', r_sq)
-
 # Print the Intercept:
-print('intercept:', model.intercept_)
+print('Intercept:', model.intercept_)
 
 # Print the Slope:
-print('slope:', model.coef_) 
+print('Slope:', model.coef_) 
 model = LinearRegression()
 model.fit(x,y)
+
+# Print the Pearson's coefficient
+pcoeff = stats.pearsonr(pX, y)
+print('Pearsons r:', pcoeff)
+
+# Obtain the coefficient of determination by calling the model with the score() function, then print the coefficient:
+r_sq = model.score(x, y)
+print('R^2/Coefficient of Determination:', r_sq)
 
 # Plot the estimated linear regression line with matplotlib:
 x_test = np.linspace(35000,100000, endpoint=True)
 y_pred = model.predict(x_test[:,None])
 
 #plots the x and y then adds the annotations for y 'states'
-plt.scatter(x, y, color = 'darkgreen')
-plt.title("Covid Infection Rates",fontsize=15)
+plt.scatter(x, y, color = 'black')
+plt.title("COVID-19 Case Counts vs. Average Income by Counties (unlabeled)", fontsize=15)
 
 #plots the prediciton and labels
 plt.xlabel("Average Income")
@@ -43,4 +49,3 @@ plt.ylabel("Case Count")
 plt.plot(x_test,y_pred, color = 'red');
 plt.legend(['Counties','Line of Regression'])
 plt.show()
-
